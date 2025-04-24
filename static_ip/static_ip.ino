ea75ee37b04b6dbfab8c1a8f4b7f384b2abf7bb3
Xavier <57724918+XavierTGN@users.noreply.github.com>
2025-04-24 10:40:37 +0200

    Versio original
    Detecta la ETHERNET

---
+++ static_ip/static_ip.ino
@@ -0,0 +1,125 @@
+/**
+ * @file      StaticIPAddress.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2023  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2023-02-17
+ *
+ */
+#include <Arduino.h>
+#if ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3,0,0)
+#include "ETHClass2.h"       //Is to use the modified ETHClass
+#define ETH  ETH2
+#else
+#include <ETH.h>
+#endif
+#include <SPI.h>
+#include <SD.h>
+#include "utilities.h"          //Board PinMap
+#include <WiFi.h>
+
+//Change to IP and DNS corresponding to your network, gateway
+IPAddress staticIP(10, 82, 103, 215);
+IPAddress gateway(10, 82, 103, 209);
+IPAddress subnet(255, 255, 255, 240);
+IPAddress dns(172,16,138,119);
+
+static bool eth_connected = false;
+
+void WiFiEvent(arduino_event_id_t event)
+{
+    switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+        Serial.println("ETH Started");
+        //set eth hostname here
+        ETH.setHostname("esp32-ethernet");
+        break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+        Serial.println("ETH Connected");
+        break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+        Serial.print("ETH MAC: ");
+        Serial.print(ETH.macAddress());
+        Serial.print(", IPv4: ");
+        Serial.print(ETH.localIP());
+        if (ETH.fullDuplex()) {
+            Serial.print(", FULL_DUPLEX");
+        }
+        Serial.print(", ");
+        Serial.print(ETH.linkSpeed());
+        Serial.println("Mbps");
+        eth_connected = true;
+        break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+        Serial.println("ETH Disconnected");
+        eth_connected = false;
+        break;
+    case ARDUINO_EVENT_ETH_STOP:
+        Serial.println("ETH Stopped");
+        eth_connected = false;
+        break;
+    default:
+        break;
+    }
+}
+
+void testClient(const char *host, uint16_t port)
+{
+    Serial.print("\nconnecting to ");
+    Serial.println(host);
+
+    WiFiClient client;
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        return;
+    }
+    client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+    while (client.connected() && !client.available())
+        ;
+    while (client.available()) {
+        Serial.write(client.read());
+    }
+
+    Serial.println("closing connection\n");
+    client.stop();
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+
+    WiFi.onEvent(WiFiEvent);
+
+#ifdef ETH_POWER_PIN
+    pinMode(ETH_POWER_PIN, OUTPUT);
+    digitalWrite(ETH_POWER_PIN, HIGH);
+#endif
+
+
+#if CONFIG_IDF_TARGET_ESP32
+    if (!ETH.begin(ETH_TYPE, ETH_ADDR, ETH_MDC_PIN,
+                   ETH_MDIO_PIN, ETH_RESET_PIN, ETH_CLK_MODE)) {
+        Serial.println("ETH start Failed!");
+    }
+#else
+    if (!ETH.begin(ETH_PHY_W5500, 1, ETH_CS_PIN, ETH_INT_PIN, ETH_RST_PIN,
+                   SPI3_HOST,
+                   ETH_SCLK_PIN, ETH_MISO_PIN, ETH_MOSI_PIN)) {
+        Serial.println("ETH start Failed!");
+    }
+#endif
+
+    if (ETH.config(staticIP, gateway, subnet, dns, dns) == false) {
+        Serial.println("Configuration failed.");
+    }
+}
+
+void loop()
+{
+    if (eth_connected) {
+        testClient("172.24.147.1", 80);
+    }
+    delay(10000);
+}
